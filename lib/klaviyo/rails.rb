03a224284c65ea52cfde782b4cdf550b1b7311be
Maxim Pechnikov <parallel588@gmail.com>
2016-04-05 13:10:56 +0300

    init gem

---
+++ lib/klaviyo/rails.rb
@@ -0,0 +1,7 @@
+require "klaviyo/rails/version"
+
+module Klaviyo
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/klaviyo/rails/version.rb
@@ -0,0 +1,5 @@
+module Klaviyo
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
