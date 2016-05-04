b8751b1d61a33cc4684c7e0f41e276779b110281
Maxim Pechnikov <parallel588@gmail.com>
2016-05-04 13:10:34 +0300

    add travis.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,4 @@ require 'klaviyo-rails'
 ActionMailer::Base.delivery_method = :klaviyo
 ActionMailer::Base.prepend_view_path(File.join(File.dirname(__FILE__), "fixtures", "views"))
 
-#Dir["#{File.dirname(__FILE__)}/fixtures/models/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/fixtures/models/*.rb"].each { |f| require f }
